aee01ee68a2daa8e74f0016f95e5096ff99aa52f
Gun44 <124424804+Gun44@users.noreply.github.com>
2026-03-23 07:46:13 +0100

    Create BaseViewModel.cs file with a base view model class implementation

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DevisConstructionApp.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set => SetProperty(ref _isLoading, value);
+        }
+
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
